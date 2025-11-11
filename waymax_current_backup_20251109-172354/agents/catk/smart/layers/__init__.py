6f001522a73c037dd78676df099464a95f17d2f7
sudo-homebrew <hyeobi@snu.ac.kr>
2025-11-11 13:40:17 +0900

    remove useless files

--- waymax_current_backup_20251109-172354/agents/catk/smart/layers/__init__.py
+++
@@ -1,3 +0,0 @@
-from waymax.agents.catk.smart.layers.attention_layer import AttentionLayer
-from waymax.agents.catk.smart.layers.fourier_embedding import FourierEmbedding, MLPEmbedding
-from waymax.agents.catk.smart.layers.mlp_layer import MLPLayer
